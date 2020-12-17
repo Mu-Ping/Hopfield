50a700875cca6a62478ab514babf8ede0da2b206
Muping <k0326jim@gmail.com>
2020-12-18 00:35:16 +0800

    complete hopfield learning

--- Hopfield.py
+++ Hopfield.py
@@ -18,7 +18,7 @@ def create_grid(c, col, row, data=None, color=True): #https://www.coder.work/art
     for y in range(row):
         for x in range(col):
             pixel = file.pop(0)
-            if(pixel==0):
+            if(pixel==-1):
                 c.create_rectangle((7*x+2, 7*y+2, 7*x+9, 7*y+9), fill = zeor_color) #pixel起始x=2 ；y=2
             elif(pixel==1):
                 c.create_rectangle((7*x+2, 7*y+2, 7*x+9, 7*y+9), fill = one_color)
@@ -48,11 +48,11 @@ def readdata():
                 row=0
                 for j in file.readlines():
                     if(row==data_format[1]): #取出一個字後參數重設定
-                        eachfile_data.append([int(i) for i in eachdata])
+                        eachfile_data.append([int(i)-1 for i in eachdata])
                         eachdata=""
                         row=0
                     else:
-                        eachdata+=j.replace(" ","0").replace("\n","")
+                        eachdata+=j.replace(" ","0").replace("\n","").replace("1","2")
                         row+=1
             data_format[2].append(eachfile_data)
 # basic_word[0] Basic_Training、basic_word[1] Basic_Testing  
@@ -66,7 +66,7 @@ def choose_test(event):
        if(event.widget.itemcget(i ,'fill')=='black'):  #取得舊有屬性
            test_data.append(1)
        elif(event.widget.itemcget(i ,'fill')=='white'):
-           test_data.append(0)
+           test_data.append(-1)
     
     if(len(test_data)%9==0): #反推canvas大小。也可以透過更改Canvas原始碼將資訊存進物件
         test_row = 12
@@ -82,23 +82,67 @@ def add_noise(test_data):
         messagebox.showwarning("Warning","請先從左方選擇測試資料")
     else:
         for i in np.random.randint(0, test_col*test_row, random.randint(3, 10)): #產生隨機幾個噪點
-            test_data[i] = 0 if test_data[i]==1 else 1 #0 1 互換
+            test_data[i] = -1 if test_data[i]==1 else 1 #-1 1 互換
             
         create_grid(test_canvas, test_col, test_row, test_data, False)
                 
 def learning():
+    global W, B
     if(data_combobox.current()==0):
-        W = np.zeros((108, 108))
-        bias = np.zeros(108)
-        train_data = np.array(basic_word[0])
-        for i in train_data:
-            W += i.reshape(-1,1).dot(i.reshape(1,-1))
-        W -= np.diag(np.diag(W)) #對角線取0
+        d = 108
+        dataset = basic_word[0]
+        bind_canvas = basic_canvas
+        unbind_canvas = bonus_canvas
+        infor = "Basic完成訓練"
     elif(data_combobox.current()==1):
-        bonus_word[0]  
+        d = 100
+        dataset = bonus_word[0][0:4]
+        bind_canvas = bonus_canvas
+        unbind_canvas = basic_canvas
+        infor = "Bonus完成訓練"
         
+    learned_data.set(infor)    
+    for c in bind_canvas: 
+        c.bind("<Enter>", focus)
+        c.bind("<Leave>", unfocus)
+        c.bind("<Button-1>", choose_test)
+    for c in unbind_canvas: 
+        c.unbind("<Enter>")
+        c.unbind("<Leave>")
+        c.unbind("<Button-1>") 
+        
+    W = np.zeros((d, d))
+    B = np.zeros((d,1))
+    train_data = np.array(dataset)
+    
+    for i in train_data:
+        W += i.reshape(-1,1).dot(i.reshape(1,-1))
+    W -= np.diag(np.diag(W)) #對角線取0
+    for i in range(d):
+        B+=W[i].reshape(-1,1)
+        
+def associate():
+    iteration_n = np.array(test_data).reshape(-1,1)
+    iternum = 0
+    while(1 and iternum<=100):
+        iteration_n1 = W.dot(iteration_n)-B
+        for i in range(len(iteration_n1)):
+            if(iteration_n1[i]>0):
+                iteration_n1[i]=1
+            elif(iteration_n1[i]<0):
+                iteration_n1[i]=-1
+            else:
+                iteration_n1[i]=iteration_n[i]
+                
+
+        if((iteration_n1==iteration_n).all()):
+            break
+        iteration_n = iteration_n1
+        iternum+=1
+    create_grid(val_canvas, test_col, test_row, list(iteration_n1), False)
+    
 def GUI():
-    global test_canvas, data_combobox
+    global test_canvas, val_canvas, data_combobox
     plt_basic_train = tk.Frame(window)
     plt_basic_train.grid(row=0, column=0)
     plt_basic_test = tk.Frame(window)
@@ -106,14 +150,14 @@ def GUI():
     plt_basic = [plt_basic_train, plt_basic_test]
     tk.Label(plt_basic_train, font=("微軟正黑體", 12, "bold"), text="Basic_Training").grid(row=0, column=0, columnspan=3)
     tk.Label(plt_basic_test, font=("微軟正黑體", 12, "bold"), text="Basic_Testing").grid(row=0, column=0, columnspan=3)
+
     for i in range(2):
         for j in range(3):
             c = tk.Canvas(plt_basic[i], height=85, width=75)
             c.grid(row=1,column=j)
             create_grid(c, 9, 12, basic_word[i][j]) #Basic資料集為9x12
-            c.bind("<Enter>", focus)
-            c.bind("<Leave>", unfocus)
-            c.bind("<Button-1>", choose_test)
+            basic_canvas.append(c)
+
 #--------------------------------------------------
     plt_bonus_train = tk.Frame(window)
     plt_bonus_train.grid(row=1, column=0, padx=20)
@@ -129,39 +173,48 @@ def GUI():
             c = tk.Canvas(plt_bonus[i], height=75, width=75)
             c.grid(row=rowindex, column=j%3)
             create_grid(c, 10, 10, bonus_word[i][j]) #Bonus資料集為10x10
-            c.bind("<Enter>", focus)
-            c.bind("<Leave>", unfocus)
-            c.bind("<Button-1>", choose_test)
+            bonus_canvas.append(c)
+
 #--------------------------------------------------
-    action_bar = tk.Frame(window)
-    action_bar.grid(row=0, column=2, rowspan=3, padx=25)
-    tk.Label(action_bar, font=("微軟正黑體", 12, "bold"), text="選擇訓練資料集").grid(row=0, column=0)
-    data_combobox = ttk.Combobox(action_bar, value=["Basic_Training","Bonus_Training"], state="readonly", width=15) #readonly為只可讀狀態
+    action_bar_1 = tk.Frame(window)
+    action_bar_1.grid(row=0, column=2, padx=25)
+    tk.Label(action_bar_1, font=("微軟正黑體", 12, "bold"), text="選擇訓練資料集").grid(row=0, column=0)
+    data_combobox = ttk.Combobox(action_bar_1, value=["Basic_Training","Bonus_Training"], state="readonly", width=15) #readonly為只可讀狀態
     data_combobox.grid(row=1, column=0, sticky=tk.W)
     data_combobox.current(0) #預設Combobox為index0
-    btn = tk.Button(action_bar, text='訓練', command=learning)
-    btn.grid(row=2, sticky=tk.E+tk.W, pady=10)
+    btn = tk.Button(action_bar_1, text='訓練', command=learning)
+    btn.grid(row=2, sticky=tk.E+tk.W, pady=5)
+    tk.Label(action_bar_1, font=("微軟正黑體", 10, "bold"), textvariable=learned_data).grid(row=3, column=0)
     
-    tk.Label(action_bar, font=("微軟正黑體", 12, "bold"), text="選擇測試資料").grid(row=3, column=0)
-    test_canvas = tk.Canvas(action_bar, height=85, width=75)
-    test_canvas.grid(row=4, column=0)
-    btn1 = tk.Button(action_bar, text='加入雜訊', command=lambda: add_noise(test_data))
-    btn1.grid(row=5, sticky=tk.E+tk.W, pady=5)
-    btn2 = tk.Button(action_bar, text='驗證')
-    btn2.grid(row=6, sticky=tk.E+tk.W, pady=5)
-    tk.Label(action_bar, font=("微軟正黑體", 12, "bold"), text="驗證結果").grid(row=7, column=0)
-    val_canvas = tk.Canvas(action_bar, height=85, width=75)
-    val_canvas.grid(row=8, column=0)
+    action_bar_2 = tk.Frame(window)
+    action_bar_2.grid(row=1, column=2, padx=25, pady=25, sticky=tk.N)
+    tk.Label(action_bar_2, font=("微軟正黑體", 12, "bold"), text="選擇測試資料").grid(row=0, column=0)
+    test_canvas = tk.Canvas(action_bar_2, height=85, width=75)
+    test_canvas.grid(row=1, column=0)
+    btn1 = tk.Button(action_bar_2, text='加入雜訊', command=lambda: add_noise(test_data))
+    btn1.grid(row=2, sticky=tk.E+tk.W, pady=5)
+    btn2 = tk.Button(action_bar_2, text='驗證', command=associate)
+    btn2.grid(row=3, sticky=tk.E+tk.W, pady=5)
+    tk.Label(action_bar_2, font=("微軟正黑體", 12, "bold"), text="驗證結果").grid(row=4, column=0)
+    val_canvas = tk.Canvas(action_bar_2, height=85, width=75)
+    val_canvas.grid(row=5, column=0)
 
 basic_word = []
 bonus_word = []
+basic_canvas=[]
+bonus_canvas=[]
 data_combobox = None
 test_canvas = None
+val_canvas = None
 test_data = None
 test_row = 0
 test_col = 0
+W = None
+B = None
+
 
 window = tk.Tk()
+learned_data = tk.StringVar()#學習率
 window.geometry("720x550")
 window.resizable(False, False)
 window.title("Hopfield")
